f62bd10a9da9ecbbe10ae20f9cb71c076ea5403f
duryb <brennandury@gmail.com>
2021-06-09 23:49:27 -0700

    Update Analyzer.py and Collector.py

--- Collector.py
+++ Collector.py
@@ -6,57 +6,71 @@ of tf-idf statistics for each word in the entire comment set for each comment.
 Vocab file maps each word to its index in tf-idf.
 """
 import re
-import numpy as np
 import pandas as pd
 from pmaw import PushshiftAPI
-from sklearn.feature_extraction.text import CountVectorizer
-import json
+import datetime
+from datetime import timedelta
+import csv
 
-limit = 900000 # The number of comments to search for. Only the comments with an mbti label are ultimately kept.
 
 nl_file_name = 'natural_language.csv'
-bag_file_name = 'term_frequencies.csv'
-vocab_file_name = 'vocab_tf.json'
+days = 365
 
 
-def _collect_data():
+def over(s):
+    return len(str(s)) > 1000
+
+
+def collect_data():
     """
     Collects comments from r/mbti and returns the data as a DataFrame.
     :return: the dataframe
     """
-    df = pd.DataFrame(columns=['text', 'E/I', 'S/N', 'T/F', 'J/P'])
     api = PushshiftAPI()
-    comments = api.search_comments(subreddit='mbti', limit=limit, fields=['body', 'author_flair_text'])
-    for comment in comments:
-        body = comment['body']
-        body = body.lower()
-        body = body.replace('\n\n', ' ')
-        body = re.sub(r'[^\w\s]', '', body)
-        flair = comment['author_flair_text']
-        if flair is not None and len(flair) == 4:
-            df.loc[len(df)] = [body, flair[0], flair[1], flair[2], flair[3]]
-    return df
+    f = open(nl_file_name, 'w', newline='')
+    writer = csv.writer(f, delimiter=',')
+    writer.writerow(['text', 'E_I', 'S_N', 'T_F', 'J_P', 'ptype'])
+    f.close()
+    comments = []
+    total_collected = 0
+    before = datetime.datetime(2021, 6, 3, 0, 0)
+    hours_dif = 12
+    after = before - timedelta(hours=hours_dif)
+    while before > datetime.datetime(2010, 12, 30, 0, 0):  # the creation of the subreddit
+        response = api.search_comments(subreddit='mbti', before=int(before.timestamp()), after=int(after.timestamp()),
+                                       fields=['body', 'author_flair_text'])
+        before = before - timedelta(hours=hours_dif)
+        comments.extend(response)
+        new_comments = len(comments) - total_collected
+        total_collected = len(comments)
+        if new_comments != 0:
+            hours_dif = (hours_dif + (hours_dif * 800 / new_comments)) / 2  # Go halfway to the estimate
+            # to get 800 on next search. If over 1000, comments will be missed. If under 1000, collection
+            # is less efficient
+            for comment in comments[-new_comments:]: #write each comment as its collected for
+                body = comment['body']
+                body = body.lower()
+                body = body.replace('\n\n', ' ')
+                body = re.sub(r'[^\w\s]', '', body)
+                flair = comment['author_flair_text']
+                if flair is not None and len(flair) == 4:
+                    g = open(nl_file_name, 'a')
+                    writer = csv.writer(g, delimiter=',')
+                    writer.writerow(pd.Series([body, flair[0], flair[1], flair[2], flair[3], flair]))
+                    g.close()
+        print(before)
+        print(hours_dif)
+        after = before - timedelta(hours=hours_dif)
 
+
+def filter_1000():
+    df = pd.read_csv(nl_file_name)
+    filter = df['text'].apply(over)
+    df['text'] = df[filter]
+    df.to_csv('1000' + nl_file_name, index=False)
 
 def main():
-    """
-    Collects the data and writes the data into the csv files.
-    """
-    df = pd.read_csv(nl_file_name)
-    #df = _collect_data()
-    #df.to_csv(nl_file_name, index=False)
-    vectorizer = CountVectorizer()
-    matrix = vectorizer.fit_transform(df['text'].apply(lambda x: np.str_(x)))
-    #with open(idf_file_name, 'w') as idf_file:
-    #    idf_file.write(json.dumps(list(vectorizer.idf_)))
-    with open(vocab_file_name, 'w') as vocab_file:
-        vocab_file.write(json.dumps(vectorizer.vocabulary_))
-    df2 = pd.DataFrame.sparse.from_spmatrix(matrix)
-    df2['E/I'] = df['E/I']
-    df2['S/N'] = df['S/N']
-    df2['T/F'] = df['T/F']
-    df2['J/P'] = df['J/P']
-    df2.to_csv(bag_file_name, index=False)
+    collect_data()
 
 
 if __name__ == '__main__':
